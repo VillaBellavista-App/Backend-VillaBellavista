0218bf403aad70e50c43c6e276fbe0951999ba6f
Caleb <reateguicevallosc@gmail.com>
2023-09-15 13:43:39 -0500

    Add: Deploying Settings

--- ProjectVilla/travels/travels/settings.py
+++ ProjectVilla/travels/travels/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import dj_database_url
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xr$8@=5!qk_b0!sovpu-)*tn!xs6c@&z-l^8c@!8uol=8m5z3*'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -79,6 +82,8 @@ WSGI_APPLICATION = 'travels.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#postgres://admin:a6Lx9gjZZrVT3kRryRXApMOSTlJGuzG6@dpg-ck2a4lvqj8ts73839r0g-a.oregon-postgres.render.com/db_villa
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -86,6 +91,9 @@ DATABASES = {
     }
 }
 
+database_url = os.environ.get("DATABASE_URL") 
+
+DATABASES["default"] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
